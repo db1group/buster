77b8e31a240996b4a544ccf1cf32e76406a25dfc
Maiko Cunha <mkacunha@pop-os.localdomain>
2020-08-18 00:17:31 -0300

    Added project to spring and jakarta-ee

---
+++ buster-jakarta-ee/src/test/java/com/db1group/buster/AppTest.java
@@ -0,0 +1,20 @@
+package com.db1group.buster;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
---
+++ buster-spring/src/main/java/com/db1group/buster/App.java
@@ -0,0 +1,13 @@
+package com.db1group.buster;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
