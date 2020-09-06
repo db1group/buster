f3c47a585e87b7d98650a33a1dde532014cb2377
Maiko Cunha <maiko.cunha@db1.com.br>
2020-09-05 22:36:46 -0300

    Config spring factory

---
+++ buster-spring/src/main/java/com/db1group/buster/BusterSpringConfiguration.java
@@ -0,0 +1,9 @@
+package com.db1group.buster;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ComponentScan(basePackages = "com.db1group.buster")
+public class BusterSpringConfiguration {
+}
